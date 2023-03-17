884c44e4c72bbff87b394e64aaf901a7f66107d3
Joao Madruga <joaop050702@hotmail.com>
2023-03-16 23:06:38 -0300

    Initial structure commit

---
+++ clustApp.swift
@@ -0,0 +1,17 @@
+//
+//  clustApp.swift
+//  clust
+//
+//  Created by João Madruga on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct clustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
