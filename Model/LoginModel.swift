450b0b5fe167f19db6cdf214ac95c502d0724d9c
Joao Madruga <joaop050702@hotmail.com>
2023-03-17 01:20:54 -0300

    Created StartScreenView

---
+++ Model/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  clust
+//
+//  Created by João Madruga on 17/03/23.
+//
+
+import Foundation
+
+struct LoginInfo: Hashable {
+    let name: String
+    let email: String
+}
